7eb0e29a37b69bd8f189acc232e527000223bf75
IzzatFirdaus <2021601074@student.uitm.edu.my>
2025-09-14 18:14:22 +0800

    fix: Add MustVerifyEmail interface to User model and configure PHPStan to treat PHPDoc types as uncertain

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -28,7 +28,7 @@ use Illuminate\Notifications\Notifiable;
  *
  * @mixin \Illuminate\Database\Eloquent\Builder
  */
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
