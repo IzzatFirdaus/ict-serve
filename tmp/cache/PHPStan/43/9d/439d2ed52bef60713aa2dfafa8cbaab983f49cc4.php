e978f3d6b13342b8cdc949ac57e655895d156e8e
IzzatFirdaus <2021601074@student.uitm.edu.my>
2025-09-19 08:07:26 +0800

    Clean up and update cache and temp files

--- tmp/cache/PHPStan/43/9d/439d2ed52bef60713aa2dfafa8cbaab983f49cc4.php
+++ tmp/cache/PHPStan/43/9d/439d2ed52bef60713aa2dfafa8cbaab983f49cc4.php
@@ -1,74 +1,55 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 // odsl-C:\XAMPP\htdocs\ict-serve\routes
-return \PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => 'v1',
-   'data' => 
-  array (
-    'C:\\XAMPP\\htdocs\\ict-serve\\routes\\api.php' => 
-    array (
-      0 => 'e137522da4b878837180a6c0c3d02976131933d5',
-      1 => 
-      array (
-      ),
-      2 => 
-      array (
-      ),
-      3 => 
-      array (
-      ),
-    ),
-    'C:\\XAMPP\\htdocs\\ict-serve\\routes\\auth.php' => 
-    array (
-      0 => 'eb3ed6380065029a2b012488e018cbef472331b8',
-      1 => 
-      array (
-      ),
-      2 => 
-      array (
-      ),
-      3 => 
-      array (
-      ),
-    ),
-    'C:\\XAMPP\\htdocs\\ict-serve\\routes\\console.php' => 
-    array (
-      0 => '07b77c6afc0aa7b564129fbb8de8f9d9f667cf50',
-      1 => 
-      array (
-      ),
-      2 => 
-      array (
-      ),
-      3 => 
-      array (
-      ),
-    ),
-    'C:\\XAMPP\\htdocs\\ict-serve\\routes\\test.php' => 
-    array (
-      0 => '41dea10aae783ff55b35896efa9f46129dbc71a6',
-      1 => 
-      array (
-      ),
-      2 => 
-      array (
-      ),
-      3 => 
-      array (
-      ),
-    ),
-    'C:\\XAMPP\\htdocs\\ict-serve\\routes\\web.php' => 
-    array (
-      0 => '9ccc5d669499e9ab0e860b0dee770e5411a735ea',
-      1 => 
-      array (
-      ),
-      2 => 
-      array (
-      ),
-      3 => 
-      array (
-      ),
-    ),
-  ),
-));
+return \PHPStan\Cache\CacheItem::__set_state([
+    'variableKey' => 'v1',
+    'data' => [
+        'C:\\XAMPP\\htdocs\\ict-serve\\routes\\api.php' => [
+            0 => 'e137522da4b878837180a6c0c3d02976131933d5',
+            1 => [
+            ],
+            2 => [
+            ],
+            3 => [
+            ],
+        ],
+        'C:\\XAMPP\\htdocs\\ict-serve\\routes\\auth.php' => [
+            0 => 'eb3ed6380065029a2b012488e018cbef472331b8',
+            1 => [
+            ],
+            2 => [
+            ],
+            3 => [
+            ],
+        ],
+        'C:\\XAMPP\\htdocs\\ict-serve\\routes\\console.php' => [
+            0 => '07b77c6afc0aa7b564129fbb8de8f9d9f667cf50',
+            1 => [
+            ],
+            2 => [
+            ],
+            3 => [
+            ],
+        ],
+        'C:\\XAMPP\\htdocs\\ict-serve\\routes\\test.php' => [
+            0 => '41dea10aae783ff55b35896efa9f46129dbc71a6',
+            1 => [
+            ],
+            2 => [
+            ],
+            3 => [
+            ],
+        ],
+        'C:\\XAMPP\\htdocs\\ict-serve\\routes\\web.php' => [
+            0 => '1e0d3398d1d46ba27a5a4f081931c1da6fe81e83',
+            1 => [
+            ],
+            2 => [
+            ],
+            3 => [
+            ],
+        ],
+    ],
+]);
